92373467130799f60e619b2cfddf65871aaea853
Felipe Rodas <ae86rodas@outlook.com>
2022-02-13 11:10:35 -0500

    corporate makeover

--- f/src/components/HeroSection/Body.js
+++ f/src/components/HeroSection/Body.js
@@ -8,7 +8,7 @@ function HeroSection() {
             {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/AI16lhbh7wY?autoplay=1&showinfo=0&controls=0&loop=1&mute=1&enablejsapi=0" frameborder="0" allowfullscreen></iframe> */}
             <video className="cover-video" src='/videos/video-2.mp4' autoPlay loop muted/>
             <h1> ADVENTURE AWAITS </h1>
-            <p> We are going back to the trenches! </p>
+            <p> This site is a work in progress </p>
             <div className="hero-btns">
                 <Button 
                 className='btns' 
@@ -22,7 +22,7 @@ function HeroSection() {
                 buttonStyle='btn--primary'
                 buttonSize='btn-large'
                 >
-                    Back to the trenches
+                    Contact me!
                 </Button>
             </div>
         </div>
