216979cd4027e9f374deaa03a71fe8c7b509827e
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-03-25 12:45:00 -0400

    more

--- f/src/components/ProjectsDisplay/ProjectsDisplay.js
+++ f/src/components/ProjectsDisplay/ProjectsDisplay.js
@@ -1,32 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ProjectsDisplay.css"
 import ProjectCard from '../ProjectCard/ProjectCard'
 
-const prj = [{
-    title: "Defiance",
-    entry: "GREATEST OF ALL TIME",
-    src:'/images/delivery.png',
-    key: 1
-},
-{
-    title: "Farm Friend",
-    entry: "helping out farms",
-    src:'/images/farm_01.jpg',
-    key: 2
-},
-{
-  title: "Genetic Algorithm Pathfinding",
-  entry: "A research project for Miami Dade College",
-  src:'/images/genetic.png',
-  key: 3
-}]
-
-
 function ProjectsDisplay() {
+
+  const [ projects, setProjects ] = useState([])
+
+  useEffect(() => {
+
+    fetch("http://localhost:8080/posts")
+    .then((res) => res.json())
+    .then((data) => setProjects(data))
+    .catch((err) => console.error(err))
+
+  }, [])
+  
   return (
       <div className='project-display-container'>
         <img className='project-display-background' alt='skyscraper background' src="https://images.unsplash.com/photo-1572883454114-1cf0031ede2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"></img>
-        {prj.map((item) => <ProjectCard props={item} key={item.key}/>)}
+        {projects.map((item) => <ProjectCard props={item} key={item.key}/>)}
       </div>
 
   )
