08ba5087456feb1a948f9a809ee92ed287437c46
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-07-14 02:37:53 -0400

    numeration

--- f/src/components/ProjectValueForm/ProjectValueForm.js
+++ f/src/components/ProjectValueForm/ProjectValueForm.js
@@ -12,7 +12,7 @@ export default () => {
         quality: 1.0
     })
 
-    const [userName, setUserName] = useState("")
+    // const [userName, setUserName] = useState("")
 
     const download = (image, { name = "img", extension = "jpg" } = {}) => {
         const a = document.createElement("a");
@@ -23,73 +23,73 @@ export default () => {
     
     const downloadScreenshot = () => takeScreenshot(ref.current).then(download);
 
-    const handleSubmit = (event) =>  {
-        alert("thank you for your message " + userName +", I will reply as soon as possible");
-        event.preventDefault();
-    }
+    // const handleSubmit = (event) =>  {
+    //     alert("thank you for your message " + userName +", I will reply as soon as possible");
+    //     event.preventDefault();
+    // }
 
     const fields = [
         ["CUSTOMER"],
-        ["identifiable", "Defined core customer", "Undefined customer"],
-        ["demographics", "clearly defined and focused", "fuzzy definition and unfocused"],
-        ["psychographics", "clearly defined and focused", "fuzzy definition and unfocused"],
+        ["1. identifiable", "Defined core customer", "Undefined customer"],
+        ["2. demographics", "clearly defined and focused", "fuzzy definition and unfocused"],
+        ["3. psychographics", "clearly defined and focused", "fuzzy definition and unfocused"],
         ["TRENDS"],
-        ["macro market", "multiple and converging", "few and disparate"],
-        ["target market", "multiple and converging", "few and disparate"],
-        ["window of opportunity", "opening", "closing"],
-        ["market structure", "emerging/fragmented", "mature/decline"],
+        ["4. macro market", "multiple and converging", "few and disparate"],
+        ["5. target market", "multiple and converging", "few and disparate"],
+        ["6. window of opportunity", "opening", "closing"],
+        ["7. market structure", "emerging/fragmented", "mature/decline"],
         ['MARKET SIZE'],
-        ["how many", "large core customer group", "small unclear customer groups"],
-        ["demand", "greater than supply", "less than supply"],
-        ['MARKET GROWTH'],
-        ["rate", "20% or greater", "less than 20%"],
+        ["8. how many", "large core customer group", "small unclear customer groups"],
+        ["9. demand", "greater than supply", "less than supply"],
+        ['10. MARKET GROWTH'],
+        ["11. rate", "20% or greater", "less than 20%"],
         ["PRICE/ FREQUENCY/ VALUE"],
-        ["price", "gross margin > 40%", "gross margin greater than 40%"],
-        ["frequency", "often and repeated", "one time"],
-        ["value", "fully reflected in price", "penetration pricing"],
-        ["operating expenses", "low and variable", "large and fixed"],
-        ["net profit margin", "greater than 10%", "lesser than 10%"],
-        ["volume", "very high", "moderate"],
+        ["12. price", "gross margin > 40%", "gross margin greater than 40%"],
+        ["13. frequency", "often and repeated", "one time"],
+        ["14. value", "fully reflected in price", "penetration pricing"],
+        ["15. operating expenses", "low and variable", "large and fixed"],
+        ["16. net profit margin", "greater than 10%", "lesser than 10%"],
+        ["17. volume", "very high", "moderate"],
         ['DISTRIBUTION'],
-        ["where are you in value chain", "high margin, high power", "low margin, low power"],
+        ["18. where are you in value chain", "high margin, high power", "low margin, low power"],
         ["COMPETITION"],
-        ["market structure", "emerging", "mature"],
-        ["number of direct competitors", "few", "many"],
-        ["number of indirect competitors", "few", "many"],
-        ["number of substitutes", "few", "many"],
-        ["steath competitors", "unlikely", "likely"],
-        ["strength of competitors", "weak", "strong"],
+        ["19. market structure", "emerging", "mature"],
+        ["20. number of direct competitors", "few", "many"],
+        ["21. number of indirect competitors", "few", "many"],
+        ["22. number of substitutes", "few", "many"],
+        ["23. steath competitors", "unlikely", "likely"],
+        ["24. strength of competitors", "weak", "strong"],
         ['KEY SUCCESS FACTORS'],
-        ["relative position", "strong", "weak"],
+        ["25. relative position", "strong", "weak"],
         ['VENDORS'],
-        ["relative power", "weak", "strong"],
-        ["gross margins they control in value chain", "low", "high"],
+        ["26. relative power", "weak", "strong"],
+        ["27. gross margins they control in value chain", "low", "high"],
         ['GOVERNMENT'],
-        ["regulations", "low", "high"],
-        ["taxes", "low", "high"],
-        ['GLOBAL ENVIRONMENT'],
-        ["customers", "interested and accessible", "not interested or accessible"],
-        ["competitors", "nonexistent or weak", "existing and strong"],
-        ["vendors", "eager", "unavaliable"]
+        ["28. regulations", "low", "high"],
+        ["29. taxes", "low", "high"],
+        ['30. GLOBAL ENVIRONMENT'],
+        ["31. customers", "interested and accessible", "not interested or accessible"],
+        ["32. competitors", "nonexistent or weak", "existing and strong"],
+        ["33. vendors", "eager", "unavaliable"]
     ]
 
 
 
-    const titles = [
-        {title: "customer", fields: 3, answer:[]}, 
-        {title: "trends", fields: 4, answer:[]}, 
-        {title: "market size", fields: 2, answer:[]}, 
-        {title: "market growth", fields: 1, answer:[]}, 
-        {title: "price/frequency/value", fields: 6, answer:[]}, 
-        {title: "distribution", fields: 1, answer:[]}, 
-        {title: "competition", fields: 6, answer:[]}, 
-        {title: "key success factors", fields: 1, answer:[]}, 
-        {title: "vendors", fields: 2, answer:[]}, 
-        {title: "government", fields: 2, answer:[]}, 
-        {title: "global envirnment", fields: 3, answer:[]}, 
-    ]
+    // const titles = [
+    //     {title: "customer", fields: 3, answer:[]}, 
+    //     {title: "trends", fields: 4, answer:[]}, 
+    //     {title: "market size", fields: 2, answer:[]}, 
+    //     {title: "market growth", fields: 1, answer:[]}, 
+    //     {title: "price/frequency/value", fields: 6, answer:[]}, 
+    //     {title: "distribution", fields: 1, answer:[]}, 
+    //     {title: "competition", fields: 6, answer:[]}, 
+    //     {title: "key success factors", fields: 1, answer:[]}, 
+    //     {title: "vendors", fields: 2, answer:[]}, 
+    //     {title: "government", fields: 2, answer:[]}, 
+    //     {title: "global envirnment", fields: 3, answer:[]}, 
+    // ]
 
-    const listItems = fields.map((field, index) => <Switch title={field[0]} pos={field[1]} neg={field[2]} />)
+    const listItems = fields.map((field, index) => <Switch title={field[0]} pos={field[1]} neg={field[2]} key={index}/>)
 
     return (
         <div className='project-value-container'>
@@ -97,7 +97,9 @@ export default () => {
                 Project Value Checklist
             </p>
             <div className='project-input-areas' ref={ref}>
-                <form className='input-form' action='http://localhost:8080/message' method='POST' onSubmit={handleSubmit}>
+                <form className='input-form' action='http://localhost:8080/message' method='POST' 
+                // onSubmit={handleSubmit}
+                >
                     {listItems}
                 </form>
             </div>
--- f/src/pages/Home.js
+++ f/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import Cards from '../components/Cards/Body';
+// import Cards from '../components/Cards/Body';
 import Footer from '../components/Footer/Body';
 import HeroSection from '../components/HeroSection/Body';
 
